01cc9dd8e87fc49c1ad2a283719e5967f0c65cab
843723683 <lz843723683@163.com>
2019-09-09 08:28:29 +0800

    1.Add "UnixbenchRunTest" and "UnixbenchInstallTest" function.Support for installation testing 2. Increase the dependence on the empty judgment 3. ./Run supports more than 16 CPU tests

--- testcases/Benchmark/unixbench.sh
+++ testcases/Benchmark/unixbench.sh
@@ -58,8 +58,10 @@ UnixbenchDep(){
 	local depTmp=""
 
 	depNum=$(echo $localDep | awk -F":" '{print NF}')
-	if [ "${depNum}" -eq "0"  ];then
-		return 0
+	if [ "${depNum}" -eq "1"  ];then
+		if [ "${localDep}" == "-" ];then
+			return 0
+		fi
 	fi
 
 	local index=0
@@ -111,14 +113,34 @@ UnixbenchInit(){
 	return $ret
 }
 
-## TODO:解析函数返回值
-## exit：1->程序退出，失败
-##     ：2->程序退出，阻塞
-UnixbenchRetParse(){
-	local tmp="$?"
-	if [ "${tmp}" -ne "0"  ];then
-		exit ${tmp}
-	fi	
+## TODO:设置Run文件最大运行的CPU
+## Out :0=>TPASS
+##	1=>TFAIL
+##      2=>TCONF
+UnixbenchSetThread(){
+	# 获取CPU个数
+	CPU_NUM=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
+	[ $? -ne 0 ] && { echo "FAIL: lscpu failed";return 2; }
+	# 判断 $CPU_NUM 是否为空 
+	[ "X$CPU_NUM" == "X" ] && { echo "FAIL: lscpu CPU(s) is NULL";return 2; }
+	# 判断 $CPU_NUM 是否为数字 
+	echo ${CPU_NUM} | grep -q '[^0-9]'
+	[ $? -eq 0 ] && { echo "FAIL:Get CPU_NUM is not digit";return 2; }
+
+	if [ $CPU_NUM -le 16 ];then
+		return 0
+	fi
+
+	echo "Set Cpu num = ${CPU_NUM}"	
+	cd ${localInstallPath}/${localFileName}
+	# 设置unixbench支持多线程
+	sed -i "s/\"System Benchmarks\", 'maxCopies' => 16/\"System Benchmarks\", 'maxCopies' => $CPU_NUM/" Run	
+	[ $? -ne 0 ] && { echo "FAIL:Set CPU_NUM to \"Run\" ";return 2; }
+	
+	cd -
+	
+	return 0
+		
 }
 
 ## TODO：安装测试工具
@@ -132,9 +154,29 @@ UnixbenchInstall(){
 	if [ "$?" -ne "0" ];then
 		echo "解压缩失败"
 		return 2
-	fi	
+	fi
+	
+	# 编译
+	cd ${localInstallPath}/${localFileName}
+	make
+	[ "$?" -ne "0" ] && return 1
+	cd -
+	
+	UnixbenchSetThread
+	ret=$?
+	[ "$ret" -ne "0" ] && return $ret
 
 	return $ret
+}
+
+## TODO:解析函数返回值
+## exit：1->程序退出，失败
+##     ：2->程序退出，阻塞
+UnixbenchRetParse(){
+	local tmp="$?"
+	if [ "${tmp}" -ne "0"  ];then
+		exit ${tmp}
+	fi	
 }
 
 ## TODO：运行测试
@@ -170,8 +212,9 @@ UnixbenchUnsetup(){
 	rm -rf ${localInstallPath}/${localFileName}
 }
 
-main(){
-	UnixbenchSetup
+## TODO:安装并且运行测试
+##
+UnixbenchRunTest(){
 	UnixbenchXMLParse
 
 	UnixbenchDep
@@ -191,6 +234,31 @@ main(){
 #	UnixbenchUnsetup
 }
 
-main
+## TODO:进行安装测试
+##
+UnixbenchInstallTest(){
+	UnixbenchXMLParse
 
-exit 0
+	UnixbenchDep
+	UnixbenchRetParse
+
+	UnixbenchInit
+	UnixbenchRetParse
+	
+	UnixbenchInstall
+	UnixbenchRetParse
+}
+
+main(){
+	UnixbenchSetup
+	
+	if [ "$#" -ne "0"  ] && [ "X$1" == "X${BENCHMARK_FLAG}" ];then
+		UnixbenchInstallTest
+	else
+		UnixbenchRunTest
+	fi
+}
+
+main $@
+
+exit $?
