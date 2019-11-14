1ccf9675b5aac28fcac586193260e825101fcf26
843723683 <lz843723683@163.com>
2019-11-14 09:22:16 +0800

    BUG(01-ntfs.sh) :Unmount the error immediately after mounting

--- testcases/FileSystems/ntfs/01-ntfs.sh
+++ testcases/FileSystems/ntfs/01-ntfs.sh
@@ -19,16 +19,16 @@
 ## TODO: 使用ctrl+c退出
 ##
 NTFS01OnCtrlC(){
-    echo "正在优雅的退出..."
-    NTFS01Clean
+	echo "正在优雅的退出..."
+	NTFS01Clean
 
-    exit ${TCONF}
+	exit ${TCONF}
 }
 
 ## TODO: 用户界面
 ##
 NTFS01USAGE(){
-    cat >&1 <<EOF
+	cat >&1 <<EOF
 --------- ntfs - 01创建/挂载ntfs文件系统 ---------
 EOF
 }
@@ -40,29 +40,29 @@ EOF
 ##        1=> Fail
 ##        other=> TCONF
 NTFS01Init(){
-    # 判断root用户
-    if [ `id -u` -ne 0 ];then
-        echo "Must use root ！"
-        exit ${TCONF}
-    fi
+	# 判断root用户
+	if [ `id -u` -ne 0 ];then
+		echo "Must use root ！"
+		exit ${TCONF}
+	fi
 
-    # 信号捕获ctrl+c
-    trap 'NTFS01OnCtrlC' INT
+	# 信号捕获ctrl+c
+	trap 'NTFS01OnCtrlC' INT
 
-    # 虚拟镜像文件名称
-    imgFile="/var/tmp/ntfs01.img"
+	# 虚拟镜像文件名称
+	imgFile="/var/tmp/ntfs01.img"
 
-    # 挂载目录
-    imgMountDir="/var/tmp/ntfs01-dir"
-    [ -d ${imgMountDir} ] && rm -rf ${imgMountDir}
-    mkdir ${imgMountDir}
-    NTFS01RetParse "Create mount dir ${imgMountDir}"
+	# 挂载目录
+	imgMountDir="/var/tmp/ntfs01-dir"
+	[ -d ${imgMountDir} ] && rm -rf ${imgMountDir}
+	mkdir ${imgMountDir}
+	NTFS01RetParse "Create mount dir ${imgMountDir}"
 
-    # 创建50MB大小的ntfs虚拟镜像文件
-    dd if=/dev/zero of=${imgFile} bs=1M count=50 &>/dev/null
-    NTFS01RetParse "Create img File ${imgFile} (50MB) "
+	# 创建50MB大小的ntfs虚拟镜像文件
+	dd if=/dev/zero of=${imgFile} bs=1M count=50 &>/dev/null
+	NTFS01RetParse "Create img File ${imgFile} (50MB) "
 
-    return ${TPASS} 
+	return ${TPASS} 
 }
 
 
@@ -72,80 +72,83 @@ NTFS01Init(){
 ##        1=> Fail
 ##        other=> TCONF
 NTFS01Test(){
-    # 虚拟成第一个未使用的回环设备
-    loopDev="$(losetup -f)"
+	# 虚拟成第一个未使用的回环设备
+	loopDev="$(losetup -f)"
 
-    losetup -f ${imgFile}
-    NTFS01RetParse "losetup -f ${imgFile}"
+	losetup -f ${imgFile}
+	NTFS01RetParse "losetup -f ${imgFile}"
     
-    # 格式化为ntfs格式
-    mkfs.ntfs ${loopDev} &>/dev/null
-    NTFS01RetParse "mkfs.ntfs ${loopDev}"
+	# 格式化为ntfs格式
+	mkfs.ntfs ${loopDev} &>/dev/null
+	NTFS01RetParse "mkfs.ntfs ${loopDev}"
 
-    # 挂载
-    mount ${loopDev} ${imgMountDir}
-    NTFS01RetParse "mount ${loopDev} ${imgMountDir}"
+	# 挂载
+	mount ${loopDev} ${imgMountDir}
+	NTFS01RetParse "mount ${loopDev} ${imgMountDir}"
 
-    # 取消挂载
-    umount ${loopDev}
-    NTFS01RetParse "umount ${loopDev}"
+	# 休眠2s，解决ntfs马上取消挂载会报错（umount: /var/tmp/ntfs01-dir：目标忙）
+	sleep 2
 
-    # 卸载回环设备
-    if [ "Z${loopDev}" != "Z" ];then
-        losetup -d ${loopDev}
-        NTFS01RetParse "losetup -d ${loopDev}"
-    fi
+	# 取消挂载
+	umount ${loopDev}
+	NTFS01RetParse "umount ${loopDev}"
 
-    return ${TPASS}
+	# 卸载回环设备
+	if [ "Z${loopDev}" != "Z" ];then
+		losetup -d ${loopDev}
+		NTFS01RetParse "losetup -d ${loopDev}"
+	fi
+
+	return ${TPASS}
 }
 
 
 ## TODO: 测试收尾清除工作
 ##
 NTFS01Clean(){
-    # 判断是否挂载
-    mount | grep -q ${loopDev}
-    if [ $? -eq 0 ];then
-        umount ${loopDev}
-    fi
+	# 判断是否挂载
+	mount | grep -q ${loopDev}
+	if [ $? -eq 0 ];then
+		umount ${loopDev}
+	fi
 
-    # 判断是否使用回环设备
-    losetup -a | grep -q ${loopDev}
-    if [ $? -eq 0 ];then
-        losetup -d ${loopDev}
-    fi
+	# 判断是否使用回环设备
+	losetup -a | grep -q ${loopDev}
+	if [ $? -eq 0 ];then
+		losetup -d ${loopDev}
+	fi
 
-    # 删除挂在目录
-    if [ -d "${imgMountDir}" ];then
-       rm -rf ${imgMountDir}
-    fi
+	# 删除挂在目录
+	if [ -d "${imgMountDir}" ];then
+		rm -rf ${imgMountDir}
+	fi
 
-    # 删除虚拟文件
-    if [ -f "${imgFile}" ];then
-        rm -rf ${imgFile}
-    fi
+	# 删除虚拟文件
+	if [ -f "${imgFile}" ];then
+		rm -rf ${imgFile}
+	fi
 
-    unset -v loopDev imgMountDir imgFile 
+	unset -v loopDev imgMountDir imgFile 
 }
 
 
 ## TODO: 解析函数返回值
 ## In  : $1 => log
 NTFS01RetParse(){
-    local ret=$?
-    local logstr=""
+	local ret=$?
+	local logstr=""
 
-    if [ $# -eq 1 ];then
-        logstr="$1"
-    fi
+	if [ $# -eq 1 ];then
+		logstr="$1"
+	fi
 
-    if [ $ret -eq 0 ];then       
-        echo "[pass] : ${logstr}"
-    else
-        echo "[fail] : ${logstr}"
-        NTFS01Clean
-        exit $TFAIL
-    fi
+	if [ $ret -eq 0 ];then       
+		echo "[pass] : ${logstr}"
+	else
+		echo "[fail] : ${logstr}"
+		NTFS01Clean
+		exit $TFAIL
+	fi
 }
 
 
@@ -155,15 +158,15 @@ NTFS01RetParse(){
 ##        1=> Fail
 ##        other=> TCONF
 NTFS01Main(){
-    NTFS01USAGE
+	NTFS01USAGE
 
-    NTFS01Init
+	NTFS01Init
 
-    NTFS01Test
+	NTFS01Test
 
-    NTFS01Clean
+	NTFS01Clean
 
-    return ${TPASS}
+	return ${TPASS}
 }
 
 
