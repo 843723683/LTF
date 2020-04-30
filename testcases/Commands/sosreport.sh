b500e773ca47c829b448f5d51b56a387cd5cd084
843723683 <lz843723683@163.com>
2020-04-30 14:23:31 +0800

    Sosreport test: add  --tmp-dir

--- testcases/Commands/sosreport.sh
+++ testcases/Commands/sosreport.sh
@@ -3,7 +3,8 @@
 
 CMD="sosreport"
 TMPFILENAME="ltfsosreport"
-TMPSOSPATH="/var/tmp/sosreport-${TMPFILENAME}"
+TMPPATHNAME="/tmp"
+TMPSOSPATH="${TMPPATHNAME}/sosreport-${TMPFILENAME}"
 ret=1
 
 echo "$0 test ${CMD}"
@@ -12,12 +13,12 @@ echo "$0 test ${CMD}"
 which ${CMD} >/dev/null 2>&1 
 [ $? -ne 0 ]&&{ echo "No command :${CMD}";exit 1; }
 
-echo "" | ${CMD} --name=${TMPFILENAME}
-[ $? -ne 0 ]&&{ echo "Failed : ${CMD} --name=${TMPFILENAME}";exit 1; }
+echo "" | ${CMD} --name=${TMPFILENAME} --tmp-dir=${TMPPATHNAME}
+[ $? -ne 0 ]&&{ echo "Failed : ${CMD} --name=${TMPFILENAME} --tmp-dir=${TMPPATHNAME}";exit 1; }
 
 # 判断文件是否存在
 ls ${TMPSOSPATH}*
-ret=$?
+[ $? -ne 0 ] && exit 1
 
 rm ${TMPSOSPATH}*
 
