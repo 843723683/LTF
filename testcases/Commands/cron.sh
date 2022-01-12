83c778c80bf0b05653cd41bfda1c8b97b6315926
843723683 <lz843723683@163.com>
2022-01-12 08:04:25 +0800

    Use New Template

--- testcases/Commands/cron.sh
+++ testcases/Commands/cron.sh
@@ -1,34 +1,86 @@
-#!/bin/bash
-# Author : Lz <lz843723683@163.com>
+#!/usr/bin/env bash
 
-CMD="run-parts"
-TEST_FILE="/etc/cron.hourly/test.sh \
+# ----------------------------------------------------------------------
+# Filename:   cron 
+# Version:    1.0
+# Date:       2021/06/25
+# Author:     Lz
+# Email:      lz843723683@gmail.com
+# History：     
+#             Version 1.0, 2021/06/25
+#             Version 2.0, 2021/12/29
+# Function:   cron 功能验证
+# Out:        
+#             0 => TPASS
+#             1 => TFAIL
+#             2 => TCONF
+# ----------------------------------------------------------------------
+
+# 测试主题
+Title_Env_LTFLIB="cron 功能测试"
+
+# 本次测试涉及的命令
+CmdsExist_Env_LTFLIB="run-parts"
+
+
+## TODO : 个性化,初始化
+#   Out : 0=>TPASS
+#         1=>TFAIL
+#         2=>TCONF
+TestInit_LTFLIB(){
+        # 创建临时文件和目录
+	testFiles="/etc/cron.hourly/test.sh \
 		/etc/cron.daily/test.sh \
 		/etc/cron.weekly/test.sh"
-ret=1
-
-echo "$0 test ${CMD}"
-
-#判断命令是否存在
-which ${CMD} >/dev/null 2>&1 
-[ $? -ne 0 ]&&{ echo "No command :${CMD}";exit 2; }
-
-for i in ${TEST_FILE}
-do
-
-cat > ${i}<<EOF
+	for i in ${testFiles}
+	do
+		cat > ${i}<<EOF
 #!/bin/bash
 echo 'test'
 EOF
-	
-	chmod a+x $i >/dev/null	
+        	CommRetParse_FailDiy_LTFLIB ${ERROR} "创建文件失败${testFile}"
+	done
 
-	run-parts $(dirname $i) | grep -q "test"
-	ret=$?
-	rm ${i}
+	return ${TPASS}
+}
 
-	[ $ret -ne 0 ]&& exit $ret
 
-done
+## TODO : 清理函数
+#   Out : 0=>TPASS
+#         1=>TFAIL
+#         2=>TCONF
+TestClean_LTFLIB(){
+	for i in ${testFiles}
+	do
+		Debug_LLE "rm -rf ${i}"
+		rm -rf ${i}
+	done
+	return ${TPASS}
+}
 
-exit $ret
+
+## TODO : 测试用例
+testcase_1(){
+	for i in ${testFiles}
+	do
+		chmod a+x $i >/dev/null	
+		run-parts $(dirname $i) | grep -q "test"
+		CommRetParse_LTFLIB "run-parts $(dirname $i) | grep -q \"test\"" "true" "no"
+	done
+}
+
+## TODO : 测试用例集
+#   Out : 0=>TPASS
+#         1=>TFAIL
+#         2=>TCONF
+Testsuite_LTFLIB(){
+	testcase_1
+
+	return $TPASS
+}
+
+
+#----------------------------------------------#
+
+source "${LIB_LTFLIB}"
+Main_LTFLIB $@
--- testcases/Commands/zip.sh
+++ testcases/Commands/zip.sh
@@ -1,34 +1,80 @@
-#!/bin/bash
-# Author : Lz <lz843723683@163.com>
+#!/usr/bin/env bash
 
-CMD="zip"
-TMPDIR="/var/tmp/dirTar"
-TMPFILE="${TMPDIR}/test-zip"
+# ----------------------------------------------------------------------
+# Filename:   yes 
+# Version:    1.0
+# Date:       2021/06/25
+# Author:     Lz
+# Email:      lz843723683@gmail.com
+# History：     
+#             Version 1.0, 2021/06/25
+#             Version 2.0, 2021/12/29
+# Function:   zip 功能验证
+# Out:        
+#             0 => TPASS
+#             1 => TFAIL
+#             2 => TCONF
+# ----------------------------------------------------------------------
 
-echo "$0 test ${CMD}"
+# 测试主题
+Title_Env_LTFLIB="zip 功能测试"
 
-#判断命令是否存在
-which ${CMD} >/dev/null 2>&1 
-[ $? -ne 0 ]&&{ echo "No command :${CMD}";exit 2; }
+# 本次测试涉及的命令
+CmdsExist_Env_LTFLIB="zip"
 
-[ -d "${TMPDIR}" ] && rm -rf ${TMPDIR}
-mkdir $TMPDIR
 
-cat > ${TMPFILE} <<EOF
-test zip
-EOF
+## TODO : 个性化,初始化
+#   Out : 0=>TPASS
+#         1=>TFAIL
+#         2=>TCONF
+TestInit_LTFLIB(){
+        # 创建临时文件和目录
+        testFile="${TmpTestDir_LTFLIB}/test-zip"
+        echo "Test zip" > ${testFile}
+        CommRetParse_FailDiy_LTFLIB ${ERROR} "创建文件失败${testFile}"
 
-zip -q /var/tmp/test-zip.zip $TMPDIR/*
-[ $? -ne 0 ] && { echo "zip failed!";exit 1; }
-rm -rf $TMPDIR
+	testZip="${TmpTestDir_LTFLIB}/test-zip.zip"
+	
+	return ${TPASS}
+}
 
-unzip -q /var/tmp/test-zip.zip -d /
-[ $? -ne 0 ] && { echo "unzip failed!";exit 1; }
 
-cat $TMPFILE | grep -q "test zip"
-[ $? -ne 0 ] && exit 1
+## TODO : 清理函数
+#   Out : 0=>TPASS
+#         1=>TFAIL
+#         2=>TCONF
+TestClean_LTFLIB(){
+	Debug_LLE "rm -rf ${testFile} ${testZip}"
+	rm -rf ${testFile} ${testZip}
+	return ${TPASS}
+}
 
-rm ${TMPDIR} -rf
-rm /var/tmp/test-zip.zip -rf
 
-exit 0
+## TODO : 测试用例
+testcase_1(){
+	zip -q ${testZip} ${testFile}
+	CommRetParse_LTFLIB "zip -q ${testZip} ${testFile}/*" "true" "no"
+	rm -rf ${testFile}
+
+	unzip -q ${testZip} -d /
+	CommRetParse_LTFLIB "unzip -q ${testZip} -d /" "true" "no"
+
+	cat ${testFile} | grep -q "Test zip"
+	CommRetParse_LTFLIB "cat ${testFile} | grep -q \"Test zip\"" "true" "no"
+}
+
+## TODO : 测试用例集
+#   Out : 0=>TPASS
+#         1=>TFAIL
+#         2=>TCONF
+Testsuite_LTFLIB(){
+	testcase_1
+
+	return $TPASS
+}
+
+
+#----------------------------------------------#
+
+source "${LIB_LTFLIB}"
+Main_LTFLIB $@
